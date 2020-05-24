8c5f08f6c6f658eacd817b2b5654de510fabadcd
fergus <fergusfettes@gmail.com>
2020-05-24 22:51:07 +0200

    added pre-commit hooks, and updated the minimal zsh

--- dockerfiles/config/minimal.zsh
+++ dockerfiles/config/minimal.zsh
@@ -19,6 +19,16 @@ alias v="vi ."
 alias gl='git log --graph --decorate --oneline $(git rev-list -g --all)'
 alias glp="git log -p"
 alias gll="git pull"
+alias ga='git add'
+alias gc='git commit -v'
+alias gcam='git commit -a -m'
+alias gcb='git checkout -b'
+alias gcm='git checkout master'
+alias gco='git checkout'
+alias gd='git diff'
+alias gm='git merge'
+alias gp='git push'
+alias grhh='git reset --hard'
 
 alias x=exit
 alias xz="exec zsh"
@@ -27,3 +37,6 @@ alias tk="tmux attach"
 
 alias l="ls -la"
 alias ll="ls"
+
+PROMPT='%F{green}%2c%F{blue} [%f '
+RPROMPT=' %F{blue}] %F{green}%D{%L:%M} %F{yellow}%D{%p}%f'
